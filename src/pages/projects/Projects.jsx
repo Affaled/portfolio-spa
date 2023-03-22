de79d4470c26b454d8cf75176897153019dc15c0
Horacio <horacio.schumann@gmail.com>
2023-03-22 18:01:58 -0300

    adicionada lista de projetos na página de projetos

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import './Projects.css'
+import ProjectView from "../../components/projectView/ProjectView";
+import "./Projects.css";
+import { projects } from "../../Data/ProjectsData";
 
 export default function Projects() {
   return (
-    <div className='projects'>
-      <div className='project-group'>
-        <h1>Projects Page.</h1>
-        <div className='text-group'>
-          <h2>My projects.</h2>
-          <p>
-          This is just a test project, therefore not a real portfolio and this tab does not have any real projects.
-          </p>
-        </div>
+    <div className="projects">
+      <h1>Projects Page.</h1>
+      <div className="text-group">
+        <h2>My projects.</h2>
+        <p>
+          This is just a test project, therefore not a real portfolio and this
+          tab does not have any real projects.
+        </p>
+      </div>
+      <div className="project-group">
+        {projects.map((project) => (
+          <ProjectView
+            description={project.description}
+            link={project.link}
+            image={project.image}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
