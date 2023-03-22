9a8c30dc4a6a9532a47eb95ce9e98306f6e4867f
Horacio <horacio.schumann@gmail.com>
2023-03-22 18:00:44 -0300

    criado mock de dados de projetos

---
+++ src/Data/ProjectsData.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    description: "Projeto 1 é muito massinha",
+    link: "https://wfdesignbuild.com",
+    image:
+      "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    description: "Projeto 2 é mais legal ainda!.",
+    link: "https://wfdesignbuild.com",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80",
+  },
+  {
+    description: "Projeto 2 é mais legal ainda!.",
+    link: "https://wfdesignbuild.com",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80",
+  },
+  {
+    description: "Projeto 2 é mais legal ainda!.",
+    link: "https://wfdesignbuild.com",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80",
+  },
+  {
+    description: "Projeto 2 é mais legal ainda!.",
+    link: "https://wfdesignbuild.com",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80",
+  },
+];
